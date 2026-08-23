d2b29e56866a395aed309e1292d74e9d6c144023
Keyvan <keyvan42@pm.me>
2026-08-24 00:58:55 +0200

    Add sitemap and page polish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://keyvaann.github.io',
   base: '/rope',
+  integrations: [sitemap()],
   output: 'static',
 });
